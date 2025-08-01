03d2bd378cef9fa5faa07f5e06af73cd2d791c10
Dhanushka <dhanushkamcr@gmail.com>
2025-08-01 17:52:46 -0500

    Inline styles

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Greeting(){
-    return <p>Welcome to the world of react!</p>
+
+    const greetingStyle = {
+        color: "green",
+        fontSize: "18px",
+        textAlign: "center"
+    };
+    return <p style={greetingStyle}>Welcome to the world of react!</p>
 }
 
 export default Greeting;
